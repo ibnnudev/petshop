526814894a0d2c43ed010ef128dcc502be644596
Moh Ibnu Abdurrohman Sutio <ibnudev@medeva.tech>
2024-07-13 19:39:32 +0700

    update hero

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -37,23 +37,31 @@
     <section>
         <div class="relative h-full py-12 mx-auto">
             <div class=" lg:text-center">
-<section class="bg-center bg-no-repeat bg-[url('https://flowbite.s3.amazonaws.com/docs/jumbotron/conference.jpg')] bg-gray-700 bg-blend-multiply">
-    <div class="px-4 mx-auto max-w-screen-xl text-center py-24 lg:py-56">
-        <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-6xl">We invest in the world’s potential</h1>
-        <p class="mb-8 text-lg font-normal text-gray-300 lg:text-xl sm:px-16 lg:px-48">Here at Flowbite we focus on markets where technology, innovation, and capital can unlock long-term value and drive economic growth.</p>
-        <div class="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0">
-            <a href="#" class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
-                Get started
-                <svg class="w-3.5 h-3.5 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-                </svg>
-            </a>
-            <a href="#" class="inline-flex justify-center hover:text-gray-900 items-center py-3 px-5 sm:ms-4 text-base font-medium text-center text-white rounded-lg border border-white hover:bg-gray-100 focus:ring-4 focus:ring-gray-400">
-                Learn more
-            </a>
-        </div>
-    </div>
-</section>
+                <section
+                    class="bg-center bg-no-repeat bg-[url('https://flowbite.s3.amazonaws.com/docs/jumbotron/conference.jpg')] bg-gray-700 bg-blend-multiply">
+                    <div class="px-4 mx-auto max-w-screen-xl text-center py-24 lg:py-56">
+                        <h1
+                            class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-6xl">
+                            Temukan Kesejahteraan Hewan <br>Peliharaan Anda</h1>
+                        <p class="mb-8 text-lg font-normal text-gray-300 lg:text-xl sm:px-16 lg:px-48">kami memahami
+                            bahwa hewan peliharaan Anda adalah bagian dari keluarga Anda</p>
+                        <div class="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0">
+                            <a href="#"
+                                class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
+                                Jelajahi sekarang
+                                <svg class="w-3.5 h-3.5 ms-2 rtl:rotate-180" aria-hidden="true"
+                                    xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                        stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
+                                </svg>
+                            </a>
+                            <a href="#"
+                                class="inline-flex justify-center hover:text-gray-900 items-center py-3 px-5 sm:ms-4 text-base font-medium text-center text-white rounded-lg border border-white hover:bg-gray-100 focus:ring-4 focus:ring-gray-400">
+                                Pelajari selengkapnya
+                            </a>
+                        </div>
+                    </div>
+                </section>
             </div>
         </div>
     </section>
@@ -61,7 +69,7 @@
         <div class="h-full max-w-6xl px-8 pb-12 mx-auto md:px-12 lg:px-32">
             <div class="px-6 mx-auto max-w-7xl lg:px-8">
                 <h2 class="text-lg font-semibold leading-8 text-center text-gray-900">
-                    Happy companies around the world.
+                    Layanan Kami
                 </h2>
                 <div
                     class="grid items-center max-w-lg grid-cols-4 mx-auto mt-10 gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-5">
