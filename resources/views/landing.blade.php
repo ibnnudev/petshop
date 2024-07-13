dbca64adb5143cc3c4a51860007ac3e8afb19a25
DwiArifRahman <ar3892513@gmail.com>
2024-07-13 19:29:49 +0700

    perubahan halaman landing page

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -35,19 +35,25 @@
         </div>
     </section>
     <section>
-        <div class="relative h-full max-w-6xl px-8 py-12 mx-auto md:px-12 lg:pt-24 lg:px-32">
-            <div class="lg:p-20 lg:text-center">
-                <p class="text-4xl font-semibold tracking-tighter text-gray-900 lg:text-6xl">
-                    Front-end development at your
-                    <span>fingertips whenever you need it and want it.</span>
-                </p>
-                <div class="flex flex-col gap-2 mt-12 md:flex-row lg:justify-center">
-                    <a href="#pricing"
-                        class="flex items-center justify-center w-auto h-10 px-4 py-2 text-sm font-semibold text-white transition-all rounded-full shrink-0 bg-accent-500 hover:bg-accent-400">See
-                        our pricing</a><a href="#services"
-                        class="flex items-center justify-center w-auto h-10 px-4 py-2 text-sm font-semibold text-gray-900 transition-all bg-gray-100 rounded-full shrink-0 hover:bg-gray-200">Checkout
-                        our services</a>
-                </div>
+        <div class="relative h-full py-12 mx-auto">
+            <div class=" lg:text-center">
+<section class="bg-center bg-no-repeat bg-[url('https://flowbite.s3.amazonaws.com/docs/jumbotron/conference.jpg')] bg-gray-700 bg-blend-multiply">
+    <div class="px-4 mx-auto max-w-screen-xl text-center py-24 lg:py-56">
+        <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-6xl">We invest in the world’s potential</h1>
+        <p class="mb-8 text-lg font-normal text-gray-300 lg:text-xl sm:px-16 lg:px-48">Here at Flowbite we focus on markets where technology, innovation, and capital can unlock long-term value and drive economic growth.</p>
+        <div class="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0">
+            <a href="#" class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
+                Get started
+                <svg class="w-3.5 h-3.5 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                </svg>
+            </a>
+            <a href="#" class="inline-flex justify-center hover:text-gray-900 items-center py-3 px-5 sm:ms-4 text-base font-medium text-center text-white rounded-lg border border-white hover:bg-gray-100 focus:ring-4 focus:ring-gray-400">
+                Learn more
+            </a>
+        </div>
+    </div>
+</section>
             </div>
         </div>
     </section>
